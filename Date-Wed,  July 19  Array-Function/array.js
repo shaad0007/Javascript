60e5cf25113e12e114b479e67373e27502bf48fb
shaad0007 <123370160+shaad0007@users.noreply.github.com>
2023-07-19 18:46:43 +0530

    july 19 work added

---
+++ Date-Wed,  July 19  Array-Function/array.js
@@ -0,0 +1,47 @@
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// arr.push(400);
+// arr.push(100000);
+// console.log(arr);
+
+// arr.pop();
+// arr.pop();
+// arr.pop();
+// arr.pop();
+// arr.pop();
+// arr.pop();
+// arr.pop();
+// arr.pop();
+// arr.pop();
+// console.log(arr);
+
+// arr.unshift(10);
+// arr.unshift(20);
+// arr.unshift(30);
+// console.log(arr);
+
+// arr.shift();
+// arr.shift();
+// arr.shift();
+// console.log(arr);
+// console.log(arr);
+// console.log(arr.toString()); // array se hata kr isee simple coma mein lekr print krdega
+
+// console.log(arr.join("$")); // ye coma ki jgh kuch bhi dusre symbol add krdeta hai
+
+// console.log(arr.reverse());
+// console.log(arr.at(-1));
+// console.log(arr.at(-2));
+// console.log(arr);
+// let arr2 = [10, 20, 30, 40, 50];
+// console.log(arr.concat(arr2));
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// arr.forEach((value) => {
+//   console.log(value);
+// });
+
+// function names(value, index) {
+//   console.log(value, index);
+// }
+// arr.forEach(names);
