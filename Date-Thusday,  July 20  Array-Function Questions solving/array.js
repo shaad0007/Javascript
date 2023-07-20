1bc514088352f9591a2d06fa738ef4fd793c966a
shaad0007 <123370160+shaad0007@users.noreply.github.com>
2023-07-20 17:38:06 +0530

    New file added

---
+++ Date-Thusday,  July 20  Array-Function Questions solving/array.js
@@ -0,0 +1,118 @@
+//Question 1. Check if the array is sorted in ascending order.
+
+// Ans 1.
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let ascd = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//   if (ascd < arr[i]) {
+//     console.log("Yes it is in Ascending order");
+//   } else {
+//     console.log("No");
+//   }
+//   ++ascd;
+// }
+
+// Question 2. Check if the array is sorted in descending order.
+
+// Ans 2.
+
+// let arr = [90, 40, 30, 10, 5, 3];
+// let des = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//   if (des > arr[i]) {
+//     console.log("Yes it is in Descending order");
+//   } else {
+//     console.log("Not ");
+//   }
+//   ++des;
+// }
+
+// Question 3. Concat two Arrays but without using concat function
+
+// Ans 3.
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let arr2 = [, 10, 20, 30, 40, 50];
+// console.log(arr + "" + arr2);
+// console.log(`${arr}${arr2}`);
+
+// Question 4. Count the total number of odd numbers in the array
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let countOdd = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 === 1) {
+//     ++countOdd;
+//   }
+// }
+// console.log(countOdd);
+
+// Question 5. Count the total number of even numbers in the array
+
+// Ans 5.
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let countEven = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 === 0) {
+//     countEven = countEven + 1;
+//   }
+// }
+// console.log(countEven);
+
+// Question 6. Create a new Array from existing Array using push function
+
+// let arr = [1, 2, 3, 4, 5, 6, "shaad Papa", 7, 8, 9];
+// arr.push(50);
+// arr.push(500);
+// arr.push(5000);
+// console.log(arr);
+
+// Question 7. Find the average of the Array
+
+// Ans 7. ???
+
+// Question 8. Find the largest word in the Array
+
+// Ans 8. ???
+
+// Question 9. Create an array of names and sort it using a sort function (do it for practice then we will discuss in the class)
+
+// Ans 9. ??
+
+// Question 10. Create an empty Array
+
+// Ans 10.
+// let arr = [];
+// console.log(arr);
+
+// Question 11. Now perform the below actions
+
+// Push 3 numbers
+// Unshift 3 numbers
+// Pop 2 numbers
+// Push 1 number
+// Unshift 1 number
+// Shift 2 numbers
+
+// Now check the result and match
+
+// Ans 11.
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// array.push(50);
+// array.push(50);
+// array.push(50);
+// console.log(array);
+// array.unshift(10);
+// array.unshift(10);
+// array.unshift(10);
+// console.log(array);
+// array.pop();
+// array.pop();
+// console.log(array);
+// array.push(500);
+// console.log(array);
+// array.unshift(500);
+// console.log(array);
+// array.shift();
+// array.shift();
+// console.log(array);
