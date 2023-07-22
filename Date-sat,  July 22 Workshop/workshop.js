e4ed063048bcf142ad85e0ffee75e108a9bd7cfa
shaad0007 <123370160+shaad0007@users.noreply.github.com>
2023-07-22 21:05:27 +0530

    new file

---
+++ Date-sat,  July 22 Workshop/workshop.js
@@ -0,0 +1,42 @@
+// let gg = 10;
+
+// let num = Number(prompt("give"));
+// if (num % 2 === 0 && num % 4 === 0 && (num < 100 || num === 84)) {
+//   console.log(num);
+// }
+
+// if (num) {
+//   confirm("true");
+// } else {
+//   confirm("false");
+// }
+
+// if (num === false) {
+//   prompt("what is your age");
+// } else {
+//   prompt("what is your name");
+// }
+
+// Question 8. Using prompt ask user about his/her age. If user is greater than 18 then show user message
+//  "Do you want to open account?" If yes click on Okay button then ask your 4 below questions
+// What is your name ?
+// What is your aaadhar number ?
+// What is your address?
+// What is your mobile number?
+// and print all these details in console. If click on cancel then show message "Sorry, please visit again."
+
+// let user = +prompt("What is your age");
+// if (user > 18) {
+//   let ans = confirm("Do you want to open account?");
+//   if (ans) {
+//     let a = prompt("What is your name ?");
+//     let b = Number(prompt("What is your aaadhar number ?"));
+//     let c = prompt("What is your address?");
+//     let d = Number(prompt("What is your mobile number?"));
+//     console.log(a, b, c, d);
+//   } else {
+//     alert("Sorry, please visit again.");
+//   }
+// } else {
+//   alert("Sorry, please visit again.");
+// }
