56cba453ff9e0419e1f09feb94439b2eb0a7e6ec
shaad0007 <123370160+shaad0007@users.noreply.github.com>
2023-07-24 18:51:47 +0530

    changes

---
+++ parctice/practice.js
@@ -0,0 +1,50 @@
+// let x = 10;
+// x = 20;
+// console.log(x);
+
+// const x = 10;
+// x = 20;
+// console.log();
+
+// var val = 300;
+// console.log(val);
+
+// var x = 10 + "20";
+// var y = "20" + 10;
+// console.log(x, y);
+
+// var w = 10 - "5";
+// var x = 10 * "5";
+// var y = 10 / "5";
+// var z = 10 % "5";
+
+// console.log(w, x, y, z);
+
+// var x = true + 2;
+// var y = false + 2;
+// console.log(x, y);
+
+// var z = 1 == true;
+// console.log(z);
+
+// let x = "20";
+// console.log(Number(x) + 20);
+
+// let y = 10;
+// console.log(String(y) + 200);
+
+// var str = "Hello World!";
+// var sliced = str.slice(-5);
+// console.log(sliced);
+
+// let str = "APPLE";
+// let lowerCase = str.toLowerCase();
+// console.log(lowerCase);
+
+// let x = 10;
+
+//  function addFive(num) {
+//   num = num + 5;
+// };
+// addFive(x);
+// console.log(a);
