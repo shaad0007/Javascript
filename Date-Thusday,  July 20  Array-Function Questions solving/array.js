081144ede7fa241241777f66ba8e24f89ca8633d
shaad0007 <123370160+shaad0007@users.noreply.github.com>
2023-07-25 15:03:58 +0530

    changes

--- Date-Thusday,  July 20  Array-Function Questions solving/array.js
+++ Date-Thusday,  July 20  Array-Function Questions solving/array.js
@@ -2,7 +2,7 @@
 
 // Ans 1.
 
-// let arr = [1, 2, 3, , 40, 4, 5, 6, 7, 8, 9];
+// let arr = [1, 2, 3,  40, 4, 5, 6, 7, 8, 9];
 // let flag = true;
 // for (let i = 0; i < arr.length - 1; i++) {
 //   let ascd = arr[i];
