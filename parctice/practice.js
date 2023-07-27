5768ac95b3ace91265d973ad9a4632749402bf21
shaad0007 <123370160+shaad0007@users.noreply.github.com>
2023-07-27 17:35:15 +0530

    new work done

--- parctice/practice.js
+++ parctice/practice.js
@@ -1,50 +1,363 @@
-// let x = 10;
-// x = 20;
-// console.log(x);
+// // let x = 10;
+// // x = 20;
+// // console.log(x);
 
-// const x = 10;
-// x = 20;
-// console.log();
+// // const x = 10;
+// // x = 20;
+// // console.log();
 
-// var val = 300;
-// console.log(val);
+// // var val = 300;
+// // console.log(val);
 
-// var x = 10 + "20";
-// var y = "20" + 10;
-// console.log(x, y);
+// // var x = 10 + "20";
+// // var y = "20" + 10;
+// // console.log(x, y);
 
-// var w = 10 - "5";
-// var x = 10 * "5";
-// var y = 10 / "5";
-// var z = 10 % "5";
+// // var w = 10 - "5";
+// // var x = 10 * "5";
+// // var y = 10 / "5";
+// // var z = 10 % "5";
 
-// console.log(w, x, y, z);
+// // console.log(w, x, y, z);
 
-// var x = true + 2;
-// var y = false + 2;
-// console.log(x, y);
+// // var x = true + 2;
+// // var y = false + 2;
+// // console.log(x, y);
 
-// var z = 1 == true;
-// console.log(z);
+// // var z = 1 == true;
+// // console.log(z);
 
-// let x = "20";
-// console.log(Number(x) + 20);
+// // let x = "20";
+// // console.log(Number(x) + 20);
 
-// let y = 10;
-// console.log(String(y) + 200);
+// // let y = 10;
+// // console.log(String(y) + 200);
 
-// var str = "Hello World!";
-// var sliced = str.slice(-5);
-// console.log(sliced);
+// // var str = "Hello World!";
+// // var sliced = str.slice(-5);
+// // console.log(sliced);
 
-// let str = "APPLE";
-// let lowerCase = str.toLowerCase();
-// console.log(lowerCase);
+// // let str = "APPLE";
+// // let lowerCase = str.toLowerCase();
+// // console.log(lowerCase);
 
-// let x = 10;
+// // let x = 10;
 
-//  function addFive(num) {
-//   num = num + 5;
-// };
-// addFive(x);
-// console.log(a);
+// //  function addFive(num) {
+// //   num = num + 5;
+// // };
+// // addFive(x);
+// // console.log(a);
+
+// // let str = "shaad";
+// // console.log(str);
+
+// // let str = new ("shaad");
+// // console.log(str);
+
+// // console.log("10 * 10 + 5");
+
+// ### Question 1
+
+// Print 1 to 10 using ++ operator
+
+// let op = 0;
+// console.log(++op);
+// console.log(++op);
+// console.log(++op);
+// console.log(++op);
+// console.log(++op);
+// console.log(++op);
+// console.log(++op);
+// console.log(++op);
+// console.log(++op);
+// console.log(++op);
+
+// ### Question 2
+
+// Print 10 to 1 using -- operator
+
+// let op = 10;
+// console.log(op--);
+// console.log(op--);
+// console.log(op--);
+// console.log(op--);
+// console.log(op--);
+// console.log(op--);
+// console.log(op--);
+// console.log(op--);
+// console.log(op--);
+// console.log(op--);
+
+// ### Question 3
+
+// Check output of following in console
+
+// - 1+1
+// - 1+200
+// - 200+"300"
+// - "learnjavascript" + 400
+
+// console.log(1 + 1) -
+//   console.log(1 + 200) -
+//   console.log(200 + "300") -
+//   console.log("learnjavascript" + 400);
+
+// ### Question 4
+
+// Check result of following
+
+// console.log(2 === 2) -
+//   console.log(2 == "2") -
+//   console.log(2 === +"2") -
+//   console.log("learnjavascript" == "learnjavascript") -
+//   console.log("LearnJavascript" == "learnjavascript") -
+//   console.log(0.3 == 0.3) -
+//   console.log(1.0 == 1) -
+//   console.log(1.0 === 1);
+
+// ### Question 5
+
+// Create 5 variables for your bank account details. It would be better if you use let, const and var all 3
+
+// let bankName = "Bank of baroda";
+// let accountNumber = 8324949467648237;
+// let ifscCode = 8579437539758;
+// let holderName = "shaad";
+// let account = "debit";
+// console.log(bankName, accountNumber, ifscCode, holderName, account);
+
+// const bankName = "Bank of baroda";
+// const accountNumber = 8324949467648237;
+// const ifscCode = 94078579437539758;
+// const holderName = "shaad";
+// const account = "debit";
+// console.log(bankName, accountNumber, ifscCode, holderName, account);
+
+// var bankName = "Bank of baroda";
+// var accountNumber = 8324949467648237;
+// var ifscCode = 94078579437539758;
+// var holderName = "shaad";
+// var account = "debit";
+// console.log(bankName, accountNumber, ifscCode, holderName, account);
+
+// ### Question
+
+// Write any code and explain that code using multi line comment
+
+/*let num = 10;
+console.log(num === 10);*/
+
+// ### Question 7
+
+// Write difference between Process and Thread
+
+// ### Question 8
+
+// Write differences between let, const and var
+
+// ### Question 9
+
+// Give 1 example of each operator with expected output:
+
+// **
+
+// let num = 2;
+// console.log(2 ** 3);
+// %
+// let num = 2;
+// console.log(2 % 7);
+// !=
+// let num = 2;
+// let num2 = 3;
+// console.log(num != num2);
+// !==
+// let num = 2;
+// let num2 = "3";
+// console.log(num !== num2);
+// <=
+// let num = 2;
+// let num2 = "3";
+// console.log(num <= num2);
+// >=
+// let num = 3;
+// let num2 = "3";
+// console.log(num >= num2);
+
+// ### Question 1
+
+// Create a variable marks and assign any number. If marks are greater than 60 then print Pass else print Fail.
+
+// let marks = 40;
+// if (marks > 60) {
+//   console.log("pass");
+// } else {
+//   console.log("fail");
+// }
+
+// ### Question 2
+
+// Take two numbers and check if both numbers are odd or not. If yes then print Odd otherwise print Even
+
+// let num = 1;
+// let num1 = 11;
+// if (num % 2 != 0 && num1 % 2 != 0) {
+//   console.log("Odd");
+// } else {
+//   console.log("Even");
+// }
+
+// ### Question 3
+
+// Take 4 variables a, b, c, d and assign their values in the way mentioned below and check output:
+
+// let a = 10;
+// let b = 50;
+// let c = 1;
+// let d = 100;
+// console.log((a = b));
+// console.log((b = c));
+// console.log((c = d));
+// console.log((d = a));
+
+// ### Question 4
+
+// Print 2's table using \*\* operator. Use any logic but it should print 2's table.
+
+// console.log(2 ** 1);
+// console.log(2 ** 2);
+// console.log(2 ** 3 - 2);
+// console.log(2 ** 3);
+// console.log(2 ** 4 - 6);
+// console.log(2 ** 4 - 4);
+// console.log(2 ** 4 - 2);
+// console.log(2 ** 4);
+// console.log(2 ** 4 + 2);
+// console.log(2 ** 4 + 4);
+
+// ### Question 5
+
+// Check output of following and validate your answers
+
+// console.log(2 + 5 * 10 - 20 ** 2);
+// console.log((20 / 5) * 10 + 10);
+
+// ### Question 6
+
+// Take a number and check if number is odd or not. If number is Odd then print Yes else print No
+
+// let num = 10;
+// if (num % 2 != 0) {
+//   console.log("Yes");
+// } else {
+//   console.log("No");
+// }
+
+// ### Question 7
+
+// Take a number and check if number is divisible by 10 && 5. If yes then print Yes else print no
+
+// let num = 2;
+// if (num % 10 === 0 && num % 5 === 0) {
+//   console.log("YES");
+// } else {
+//   console.log("NO");
+// }
+// ### Question 8
+
+// Take a number and check if number \* 20 makes it 100 or not. If yes the print Yes else print No
+
+// let num = 20;
+// if (num * 5 === 100) {
+//   console.log("Yes");
+// } else {
+//   console.log("no");
+// }
+
+// ### Question 9
+
+// Calculate your 10th percentage and print Pass or Fail if marks are more than 70 then print Pass else print Fail
+
+// let marks = 880;
+// let totalMarks = 1000;
+// let per = (marks / totalMarks) * 100;
+// if (per > 70) {
+//   console.log("PASS");
+// } else {
+//   console.log("FAIL");
+// }
+
+// ### Question 10
+
+// Take your age and check if your age is greater than 21 then print Adult else print Minor
+
+// let myAge = 22;
+// if (myAge > 21) {
+//   console.log("Adult");
+// } else {
+//   console.log("Minor");
+// }
+
+// ### Question 11
+
+// Convert all if-else solutions to ternary operator solution. So instead of using if-else, use ternary operator.
+// let myAge = 22;
+// let ans = myAge > 21 ? console.log("Adult") : console.log("MInor");
+// same ans in both statement
+// console.log(ans);
+
+// ### Question 12
+
+// Implement mark-sheet logic
+
+// ```
+// Take a field percentage
+// If percentage is >= 80 then print Distinction
+// If percentage is >= 60 and < 80 then print First division
+// If percentage >= 33 and < 60 then print Second division
+// if percentage < 33 then print Fail
+// ```
+
+// let per = 5;
+// if (per >= 80) {
+//   console.log("Distinction");
+// } else if (per >= 60 && per < 80) {
+//   console.log("First division");
+// } else if (per >= 33 && per < 60) {
+//   console.log("Second division");
+// } else if (per < 33) {
+//   console.log("Fail");
+// } else {
+//   console.log("Invalid Number");
+// }
+
+// ### Question 13
+
+// Find maximum of 2 numbers
+
+// let num1 = 100;
+// let num2 = 100;
+// if (num1 > num2) {
+//   console.log("Num1 is Greater");
+// } else if (num2 > num1) {
+//   console.log("Num2 is Greater");
+// } else {
+//   console.log("both are equal");
+// }
+
+// ### Question 14
+
+// Find maximum of 3 numbers
+
+// let num1 = 100;
+// let num2 = 200;
+// let num3 = 300;
+// if (num1 > num2 && num1 > num3) {
+//   console.log("Num1 is Greater");
+// } else if (num2 > num1 && num2 > num3) {
+//   console.log("Num2 is Greater");
+// } else if (num3 > num2 && num3 > num1) {
+//   console.log("Num3 is Greater");
+// } else {
+//   console.log("both are equal");
+// }
