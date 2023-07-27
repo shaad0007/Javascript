7e12becb5f0fa12795c87ba7b3700c048db767c9
shaad0007 <123370160+shaad0007@users.noreply.github.com>
2023-07-27 14:15:08 +0530

    new file added

--- Date-Wed,  July 26  Array - function/array.js
+++ Date-Wed,  July 26  Array - function/array.js
@@ -34,6 +34,75 @@
 // });
 // console.log(ans);
 
-const arr = [1, 2, [3, 4], [5, [6, 7]]];
-const flattenedArr = arr.flat(5); // [1, 2, 3, 4, 5, 6, 7]
-console.log(flattenedArr);
+// const arr = [1, 2, [3, 4], [5, [6, 7]]];
+// const flattenedArr = arr.flat(5); // [1, 2, 3, 4, 5, 6, 7]
+// console.log(flattenedArr);
+
+// let arr = [20, 40, 1, 3, 6, 7, 34, 5];
+// let ans = arr.reduce((pre, curr) => {
+//   return pre + curr;
+// }, 0);
+// console.log(ans);
+
+// let arr = ["shaad", "sehfeen", "shoreb", "rahul", "undertaker"];
+// let ans = arr.reduce((pre, curr) => {
+//   if (curr.length > 5) {
+//     return pre + 1;
+//   } else {
+//     return pre;
+//   }
+// }, 0);
+// console.log(ans);
+
+// let arr = ["shaad", "sehfeen", "shoreb", "rahul", "undertaker"];
+// let ans = arr.filter((value) => {
+//   return value.length > 5;
+// });
+// console.log(ans.length);
+
+// let arr = [1, 2, 3, 4, 5];
+// let ans = arr.reduce((pre, curr) => {
+//   if (curr % 2 === 1) {
+//     return pre + 1;
+//   } else {
+//     return pre;
+//   }
+// }, 0);
+
+// console.log(ans);
+
+// console.log(ans);
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// const oddNumbers = arr.reduce((accumulator, current) => {
+//   if (current % 2 !== 0) {
+//     accumulator.push(current);
+//   }
+//   return accumulator;
+// }, []);
+
+// console.log(oddNumbers); // Output: [1, 3, 5, 7, 9]
+
+// let arr = ["abscdhjhihiufhiuesghuifh", "jhfhds", "hfuysdhf", "nchhhh"];
+// let ans = arr.reduce((pre, curr) => {
+//   if (curr.length > 5 && curr.length < 10) {
+//     return pre + 1;
+//   } else {
+//     return pre;
+//   }
+// }, 0);
+
+// console.log(ans);
+
+// let arr = ["d", "a", "c"];
+// let ans = arr.sort();
+// console.log(ans);
+
+// console.log(arr);
+
+// let arr = [1, 2, 3, [10, 11, [15, 16], 12, 13], 4, 5, 6, 7, 8, 9];
+// let arr2 = [arr, 20, 30, 40, 50];
+// console.log(arr2.flat());
+// console.log(arr2[0][6]);
+// console.log(arr[3][2][1]);
---
+++ Date-Wed,  July 26  Array-function and object/script.js
@@ -0,0 +1,30 @@
+// let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+// let ans = arr.slice(-4);
+// console.log(ans);
+
+// arr.splice(4, 2, 300, 400);
+// console.log(arr);
+
+// agr variable mein store kr rhe hai to ans alg aa rha hai
+
+// let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+// let ans = arr.findIndex((value) => {
+//   return value === 6;
+// });
+
+// console.log(ans);
+
+// arr.splice(ans, 1);
+// console.log(arr);
+
+// let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+// arr.splice(0, 2);
+// console.log(arr);
+
+// let obj = {
+//   aadhar: 64826428463864,
+//   fees: 2000,
+//   address: "idraNagar",
+// };
+// console.log(obj);
