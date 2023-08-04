3f029d077497a9a64b62173d32a4dd0507f4b764
shaad0007 <123370160+shaad0007@users.noreply.github.com>
2023-08-04 13:21:35 +0530

    changes

---
+++ Run car by javascript code/script.js
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        *{
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+        body{
+            height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        .container{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+        .city{
+            background-image: url(https://i.imgur.com/Q6bTT2k.png);
+            width: 100vh;
+            height: 36vh;
+            background-repeat: no-repeat;
+        }
+        .highway{
+            background-image: url(https://i.imgur.com/bVXQ8P5.jpg);
+            margin-top: -1px;
+            width: 400vh;
+            height: 6vh;
+            background-size: contain;
+            background-repeat: repeat;
+            animation: highway 5s linear infinite;
+        }
+        @keyframes highway{
+            100%{
+                transform: translateX(-900px);
+            }
+        }
+        .car{
+            width: 200px;
+            margin-top: -90px;
+            z-index: 1;
+        }
+        .car img{
+            max-width: 100%;
+            max-height: 100%;
+            animation: car 1s linear infinite;
+            
+        }
+        .wheel{
+            width: 200px;
+            display: flex;
+            justify-content: space-between;
+            padding: 0 25px 0 18px;
+            margin-top: -44px;
+        }
+        .wheel img{
+            width: 35px;
+            height: 35px;
+            animation: spin 2s linear infinite;
+        }
+        @keyframes spin{
+            100%{
+                transform: rotate(360deg);
+            }
+        }
+        @keyframes car{
+            0%{
+                transform: translateY(-2px);
+            }
+            50%{
+                transform: translateY(0);
+            }
+            100%{
+                transform: translateY(-2px);
+            }
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="city"></div>
+        <div class="highway"></div>
+        <div class="car">
+            <img src="https://i.imgur.com/n947rWL.png" alt="">
+        </div>
+        <div class="wheel">
+            <img src="https://i.imgur.com/uZh01my.png" alt="">
+            <img src="https://i.imgur.com/uZh01my.png" alt="">
+        </div>
+    </div>
+</body>
+</html>
