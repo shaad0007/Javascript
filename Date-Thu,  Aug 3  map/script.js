63804d734ab5e0a41e7743513a9f452ed054ff9d
shaad0007 <123370160+shaad0007@users.noreply.github.com>
2023-08-07 19:02:53 +0530

    changes

--- Date-Thu,  Aug 3  map/script.js
+++ Date-Thu,  Aug 3  map/script.js
@@ -249,3 +249,10 @@
 //   }
 // }
 // console.log(map);
+
+// for (let i = 1; i <= 5; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     document.write("*");
+//   }
+//   document.write("<br>");
+// }
