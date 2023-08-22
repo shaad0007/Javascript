544ff4d5488dc671cd3b52e0fac36c3e97a9c209
shaad0007 <123370160+shaad0007@users.noreply.github.com>
2023-08-22 12:33:44 +0530

    changes

--- Date-Mon,  Aug 21 DOM/script.js
+++ Date-Mon,  Aug 21 DOM/script.js
@@ -1,10 +1,12 @@
-console.log(document);
-let pTag = document.getElementById("hello");
-document.getElementsByClassName("test");
-pTag.style.backgroundColor = "purple";
-pTag.style.color = "green";
-pTag.style.fontSize = "50px";
-pTag.innerHTML = "<i>Hello yarr bhai</i>";
+// console.log(document);
+// let pTag = document.getElementById("hello");
+// document.getElementsByClassName("test");
+// pTag.style.backgroundColor = "purple";
+// pTag.style.color = "green";
+// pTag.style.fontSize = "50px";
+// pTag.innerHTML = "<i>Hello yarr bhai</i>";
 // document.getElementsByClassName("test").textContent = "python";
 // document.getElementById("test")[1].style.backgroundColor = "red";
-document.getElementsByTagName("span").;
+// document.getElementsByTagName("span").;
+
+console.log(document);
