72f20075b7785526527135f422b6e8eaf3d9ac03
shaad0007 <123370160+shaad0007@users.noreply.github.com>
2023-08-22 19:02:28 +0530

    new file

--- Date-Mon,  Aug 21 DOM/script.js
+++ Date-Mon,  Aug 21 DOM/script.js
@@ -10,3 +10,23 @@
 // document.getElementsByTagName("span").;
 
 console.log(document);
+let pTag = document.getElementById("hello");
+pTag.style.color = "red";
+pTag.style.backgroundColor = "cyan";
+pTag.style.fontSize = "50px";
+pTag.innerHTML = "<i>Mein to papa hu</i>";
+document.getElementById("text").innerText = "what the hell";
+document.getElementsByClassName("test")[0].style.backgroundColor = "green";
+document.getElementsByTagName("span")[1].style.fontSize = "100px";
+document.querySelector(".wecode").style.color = "cyan";
+// document.querySelectorAll(".wecode")[0].style.color = "green";
+// document.querySelectorAll(".wecode")[1].style.color = "green";
+for (let value of document.querySelectorAll(".wecode")) {
+  value.style.color = "red";
+}
+console.log(document.baseURI);
+// setInterval(() => {
+//   pTag.hidden = !pTag.hidden;
+// }, 1000);
+
+// console.log(document.getElementsByClassName("test"));
