ca39841bf65cb995b98a551dff0741f678fd891c
shaad0007 <123370160+shaad0007@users.noreply.github.com>
2023-09-05 08:11:49 +0530

    new file

--- Date-Friday,  Sep 1 Workshop/script.js
+++ Date-Friday,  Sep 1 Workshop/script.js
@@ -1,42 +1,4 @@
-// const obj = {
-//   name: "shaad",
-//   city: "Indore",
-//   state: "M.P",
-//   age: 40,
-//   fullName: function () {
-//     return this.name + " " + this.age;
-//   },
-//   fav: ["one", "two", "three", "four"],
-// };
-
-// console.log(obj["fav"][0]);
-// console.log(obj.fullName());
-// console.log();
-// document.write(obj);
-
-// const arr = [1, 2, 3, 4, 5];
-// arr[1] = 10;
-// console.log(arr);
-
-// let student = [
-//   { name: "shaad", age: 49, city: "nimbahera" },
-//   { name: "purvi", age: 20, city: "indore" },
-//   { name: "nayak", age: 51, city: "Jaipur" },
-// ];
-
-// for (let a = 0; a < student.length; a++) {
-//   console.log(student[a].name + " " + student[a].age);
-// }
-
-// document.write(student[0].city);
-// console.log(Math.cbrt(125));
-
-// document.getElementById("demo").innerText = "<i>Fuck</i>";
-
-// let arr = [1, 2, 3, 4, 5];
-// arr.map((value) => {
-//   return value * 10;
-// });
-
-// let date = new Date();
-// console.log(`${date.getDate()}:${date.getMonth()}:${date.getFullYear()}`);
+let element;
+document.getElementById("header").innerHTML = "<h2>Shaad</h2>";
+element = document.getElementById("header");
+console.log(element);
