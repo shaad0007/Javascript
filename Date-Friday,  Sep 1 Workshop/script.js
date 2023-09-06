c5ea7ddb3091a59daac5d1c385b1d64856bb0871
shaad0007 <123370160+shaad0007@users.noreply.github.com>
2023-09-06 20:45:00 +0530

    changes

--- Date-Friday,  Sep 1 Workshop/script.js
+++ Date-Friday,  Sep 1 Workshop/script.js
@@ -1,6 +1,10 @@
-let a = 0;
-setInterval((shift) => {
-  a += 10;
-  let target = document.getElementById("div");
-  target.style.marginLeft = a + "px";
-}, 1000);
+// let a = 0;
+// let strat = setInterval((shift) => {
+//   a += 10;
+//   let target = document.getElementById("div");
+//   target.style.marginLeft = a + "px";
+// }, 1000);
+// clearInterval();
+
+let a = "shaad";
+let b = "";
