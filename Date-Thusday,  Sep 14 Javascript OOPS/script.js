b918e62c0b64b2ca0ca36e4a734b60da987c7c18
shaad0007 <123370160+shaad0007@users.noreply.github.com>
2023-09-21 22:36:02 +0530

    changes

--- Date-Thusday,  Sep 14 Javascript OOPS/script.js
+++ Date-Thusday,  Sep 14 Javascript OOPS/script.js
@@ -1,7 +1,42 @@
-function running(name) {
-  console.log(`${name} running`);
-}
+// function running(name) {
+//   console.log(`${name} running`);
+// }
 
-running("Dog");
+// running("Dog");
 
-console.log("shaad");
+let animal = function (name, legs, color) {
+  console.log(name, legs, color);
+  this.name = name;
+  this.legs = legs;
+  this.color = color;
+  this.eat = function () {
+    console.log(`${this.name} eating`);
+  };
+  this.sleep = function () {
+    console.log(`${this.name} sleeping`);
+  };
+};
+
+let lion = new animal("lion", 4, "red");
+let parrot = new animal("parrot", 2, "green");
+let Dog = new animal("Dog", 6, "yellow");
+console.log(lion, parrot, Dog);
+
+// let account = function (
+//   name,
+//   bankName,
+//   addharcardNo,
+//   mobile,
+//   address,
+//   currentbalance
+// ) {
+//   this.name = name;
+//   this.bankName = bankName;
+//   this.addharcardNo = addharcardNo;
+//   this.mobile = mobile;
+//   this.namaddress = address;
+//   this.currentbalance = currentbalance;
+// };
+
+// let HDFC = new account("shaad", 89432798427, 2397498, "shanti nagar", 50000);
+// console.log(HDFC);
